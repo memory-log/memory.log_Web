c13b4c9d09d6f9b1f9ae1dfeb5108d7165aed8e7
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-09 16:36:20 +0900

    [FIX] Style

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Main.scss";
 import people from "../../assets/images/people.svg";
 import Button from "../common/Material/Button";
+import FadeIn from "react-fade-in";
 
 const Main = () => {
   return (
@@ -9,13 +10,21 @@ const Main = () => {
       <div className="Main">
         <div className="Main-Container">
           <div className="Main-Container-Text">
-            <p>남기고 싶은 추억을</p>
-            <p>롤링페이퍼에 작성해보세요.</p>
+            <FadeIn delay={100}>
+              <p>남기고 싶은 추억을</p>
+              <p>롤링페이퍼에 작성해보세요.</p>
+            </FadeIn>
           </div>
-          <img src={people} alt={people} />
-          <Button variant="contained" onClick={() => {}}>
-            롤링페이퍼 시작하기
-          </Button>
+          <FadeIn delay={400}>
+            <img className="Main-Container-Image" src={people} alt={people} />
+          </FadeIn>
+        </div>
+        <div className="Main-Button">
+          <FadeIn delay={500}>
+            <Button variant="contained" onClick={() => {}}>
+              롤링페이퍼 시작하기
+            </Button>
+          </FadeIn>
         </div>
       </div>
     </>
