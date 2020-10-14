9c6586be49c1ad2985bccd6222d5859cb24e83f3
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-14 18:26:14 +0900

    [UPDATE] Default Prop Value on Material Components

--- src/components/common/Material/MaterialTextField.tsx
+++ src/components/common/Material/MaterialTextField.tsx
@@ -1,41 +1,37 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button, { ButtonProps as MuiButtonProps } from "@material-ui/core/Button";
+import TextField, { TextFieldProps } from "@material-ui/core/TextField";
 import { Omit } from "@material-ui/types";
 
 interface Props {
   color?: string;
   fontWeight?: number;
   fontSize?: string;
-  backgroundColor?: string;
   hoverColor?: string;
   width?: string;
   height?: string;
-  borderRadius?: string;
 }
 
 const useStyles = makeStyles({
   root: {
-    color: (props: Props) => props.color || "#ffffff",
-    backgroundColor: (props: Props) => props.backgroundColor || "#3C50E0",
     fontWeight: (props: Props) => props.fontWeight || 500,
     fontSize: (props: Props) => props.fontSize || "0.9rem",
-    borderRadius: (props: Props) => props.borderRadius || "0.4rem",
     width: (props: Props) => props.width || "18rem",
     height: (props: Props) => props.height || "2.7rem",
     fontFamily: "Noto Sans KR",
-    boxShadow: "none",
-    "&:hover": {
-      boxShadow: "none",
-      backgroundColor: (props: Props) => props.hoverColor || "#5669F3"
+    "& label.Mui-focused": {
+      color: (props: Props) => props.color || "grey"
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: (props: Props) => props.color || "grey"
     }
   }
 });
 
-const MaterialButton = (props: Props & Omit<MuiButtonProps, keyof Props>) => {
+const MaterialTextField = (props: Props & Omit<TextFieldProps, keyof Props>) => {
   const { color, ...other } = props;
   const classes = useStyles(props);
-  return <Button className={classes.root} {...other} />;
+  return <TextField className={classes.root} {...other} />;
 };
 
-export default MaterialButton;
+export default MaterialTextField;
