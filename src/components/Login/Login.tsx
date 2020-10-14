284bf8d80f6d607a93e8a56a2a840aa493c15981
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-14 20:55:55 +0900

    [FIX] Fade In Animation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import MaterialTextField from "../common/Material/MaterialTextField";
 import MaterialButton from "../common/Material/MaterialButton";
 import "./Login.scss";
@@ -12,30 +12,40 @@ const Login = () => {
     <>
       <div className="Login">
         <div className="Login-Title">
-          <div className="Login-Title-Id">
-            <p>제 아이디는</p>
-            <div className="Login-Title-Id-TextField">
-              <MaterialTextField id="standard-password-input" label="아이디" type="text" width="9rem" height="1.4rem" />
+          <FadeIn delay={100}>
+            <div className="Login-Title-Id">
+              <p>제 아이디는</p>
+              <div className="Login-Title-Id-TextField">
+                <MaterialTextField id="standard-password-input" label="아이디" type="text" width="9rem" height="1.4rem" />
+              </div>
+              <p>이며,</p>
             </div>
-            <p>이며,</p>
-          </div>
-          <div className="Login-Title-Password">
-            <p>비밀번호는</p>
-            <div className="Login-Title-Password-TextField">
-              <MaterialTextField id="standard-password-input" label="비밀번호" type="password" width="10rem" height="1.4rem" />
+          </FadeIn>
+          <FadeIn delay={300}>
+            <div className="Login-Title-Password">
+              <p>비밀번호는</p>
+              <div className="Login-Title-Password-TextField">
+                <MaterialTextField id="standard-password-input" label="비밀번호" type="password" width="10rem" height="1.4rem" />
+              </div>
+              <p className="Login-Title-Password-End">입니다.</p>
             </div>
-            <p className="Login-Title-Password-End">입니다.</p>
+          </FadeIn>
+        </div>
+        <FadeIn delay={500}>
+          <div className="Login-Image">
+            <img src={lock} alt={lock} />
           </div>
-        </div>
-        <div className="Login-Image">
-          <img src={lock} alt={lock} />
-        </div>
+        </FadeIn>
         <div className="Login-Button">
-          <MaterialButton>로그인</MaterialButton>
-          <div className="Login-Button-Text">
-            <p className="Login-Button-Text-Comment">앗 계정이 없는데 어떡하죠?</p>
-            <p className="Login-Button-Text-Register">회원가입</p>
-          </div>
+          <FadeIn delay={700}>
+            <MaterialButton>로그인</MaterialButton>
+          </FadeIn>
+          <FadeIn delay={800}>
+            <div className="Login-Button-Text">
+              <p className="Login-Button-Text-Comment">앗 계정이 없는데 어떡하죠?</p>
+              <p className="Login-Button-Text-Register">회원가입</p>
+            </div>
+          </FadeIn>
         </div>
       </div>
     </>
