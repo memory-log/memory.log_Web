9419b9044640d8b766caef8b6513d4c6dd4c9e28
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-29 10:51:23 +0900

    [DELETE] Register Container, Page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "../pages/MainPage";
-import RegisterPage from "../pages/RegisterPage";
 import EmailAuth from "./EmailAuth";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={MainPage} />
-        <Route path="/register" component={RegisterPage} />
         <Route path="/email" component={EmailAuth} />
       </Switch>
     </div>
