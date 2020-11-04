48dd59430964dff3eb7e61a7598392f98a8eccfc
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-04 17:58:11 +0900

    [FIX] Header, Auth Modal

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -4,14 +4,18 @@ import close from "../../../assets/images/close.svg";
 
 interface ModalProps {
   handleClose: () => void;
+  isModalSelected: boolean;
   children: React.ReactNode;
 }
 
-const Modal = ({ handleClose, children }: ModalProps) => {
+const Modal = ({ handleClose, isModalSelected, children }: ModalProps) => {
   return (
     <div className="Modal">
-      <div className="Modal-Background" onClick={handleClose}></div>
-      <div className="Modal-Box">
+      <div
+        className={isModalSelected ? "Modal-Background-Enable Modal-Background" : "Modal-Background-Disable Modal-Background"}
+        onClick={handleClose}
+      ></div>
+      <div className={isModalSelected ? "Modal-Box-Enable Modal-Box" : "Modal-Box-Disable Modal-Box"}>
         <img src={close} alt={close} onClick={handleClose} />
         {children}
       </div>
