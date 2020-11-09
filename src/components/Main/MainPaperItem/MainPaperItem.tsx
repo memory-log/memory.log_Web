9b6536b779bd8e7461e7f6cdd92a401014f595e1
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-09 19:45:39 +0900

    [ADD] Reactive Design

--- src/components/Main/MainPaperItem/MainPaperItem.tsx
+++ src/components/Main/MainPaperItem/MainPaperItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./MainPaperItem.scss";
-import like from "../../../assets/images/like.svg";
+import { ReactComponent as Like } from "../../../assets/images/like.svg";
 
 interface MainPaperItemProps {}
 
@@ -8,27 +8,23 @@ const MainPaperItem = ({}: MainPaperItemProps) => {
   return (
     <>
       <div className="Main-Paper-Item">
-        <div className="Main-Paper-Item-Container">
-          <div className="Main-Paper-Item-Container-Image"></div>
-          <div className="Main-Paper-Item-Container-Info">
-            <div className="Main-Paper-Item-Container-Info-Content">
-              <p className="Main-Paper-Item-Container-Info-Content-Date">2020년 11월 3일</p>
-              <p className="Main-Paper-Item-Container-Info-Content-Title">동해물과 백두산이 마르고 닳도록 하느님이dsadasdasd</p>
-              <p className="Main-Paper-Item-Container-Info-Content-Description">
-                남산 위에 저 소나무 철갑을 두른 듯 바람서리 불변함은dsadadasdadsaddsadsadasdasdasdsasdsasdasdasddasd
-              </p>
+        <div className="Main-Paper-Item-Image"></div>
+        <div className="Main-Paper-Item-Info">
+          <div className="Main-Paper-Item-Info-Content">
+            <p className="Main-Paper-Item-Info-Content-Date">2020년 11월 3일</p>
+            <p className="Main-Paper-Item-Info-Content-Title">이것은 테스트용 타이틀입니다 승호야 나 잘했찌 낌모링</p>
+            <p className="Main-Paper-Item-Info-Content-Description">그리고 이거는 테스트용 설명이야요 이게 나야</p>
+          </div>
+          <div className="Main-Paper-Item-Info-Bottom">
+            <div className="Main-Paper-Item-Info-Bottom-Profile">
+              <div className="Main-Paper-Item-Info-Bottom-Profile-Avatar"></div>
+              <span className="Main-Paper-Item-Info-Bottom-Profile-Name">
+                <span>To. </span>홍길동
+              </span>
             </div>
-            <div className="Main-Paper-Item-Container-Info-Bottom">
-              <div className="Main-Paper-Item-Container-Info-Bottom-Profile">
-                <div className="Main-Paper-Item-Container-Info-Bottom-Profile-Avatar"></div>
-                <span className="Main-Paper-Item-Container-Info-Bottom-Profile-Name">
-                  <span>To. </span>홍길동
-                </span>
-              </div>
-              <div className="Main-Paper-Item-Container-Info-Bottom-Like">
-                <img className="Main-Paper-Item-Container-Info-Bottom-Like-Icon" src={like} alt={like} />
-                <span className="Main-Paper-Item-Container-Info-Bottom-Like-Count">30</span>
-              </div>
+            <div className="Main-Paper-Item-Info-Bottom-Like">
+              <Like className="Main-Paper-Item-Info-Bottom-Like-Icon" />
+              <span className="Main-Paper-Item-Info-Bottom-Like-Count">30</span>
             </div>
           </div>
         </div>
