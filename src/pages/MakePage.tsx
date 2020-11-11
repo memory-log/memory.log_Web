34751e096a6a1020e068c247c6dece01fb17e08e
Jominhui <rihito2021@naver.com>
2020-11-11 13:38:24 +0900

    [FIX] Color templete

---
+++ src/pages/MakePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderContainer from "../containers/Header/HeaderContainer";
+import MakeContainer from "../containers/Make/MakeContainer";
+
+const MakePage = () => {
+  return (
+    <>
+      <HeaderContainer />
+      <MakeContainer />
+    </>
+  );
+};
+
+export default MakePage;
