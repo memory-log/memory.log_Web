ab67681db83e83528af55a56fe4e4cad2e586643
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-14 18:52:07 +0900

    [ADD] Search Paper Container

--- src/components/Paper/SearchPaper/SearchPaper.tsx
+++ src/components/Paper/SearchPaper/SearchPaper.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import PaperType from "../../../util/types/Paper";
 import "./SearchPaper.scss";
 
-interface SearchPaperProps {}
+interface SearchPaperProps {
+  papers: PaperType[];
+  loading: boolean;
+  notFound: boolean;
+  setTarget: React.Dispatch<React.SetStateAction<string>>;
+}
 
-const SearchPaper = ({}: SearchPaperProps) => {
+const SearchPaper = ({ papers, loading, notFound, setTarget }: SearchPaperProps) => {
   return (
     <>
       <div className="Search-Paper">
         <div className="Search-Paper-Container">
-          <input className="Search-Paper-Container-Bar" placeholder="검색어를 입력해주세요." />
+          <input
+            className="Search-Paper-Container-Bar"
+            placeholder="검색어를 입력해주세요."
+            onChange={(e) => {
+              setTarget(e.target.value);
+            }}
+          />
+          <button onClick={() => {}}>검색띠</button>
         </div>
       </div>
     </>
--- src/containers/Paper/SearchPaper/SearchPaperContainer.tsx
+++ src/containers/Paper/SearchPaper/SearchPaperContainer.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import SearchPaper from "../../../components/Paper/SearchPaper/SearchPaper";
+import PaperType from "../../../util/types/Paper";
+import useStore from "../../../lib/hooks/useStore";
+
+interface SearchPaperResponse {
+  status: number;
+  message: string;
+  data: {
+    SearchedByName: PaperType[];
+    SearchedByTitle: PaperType[];
+  };
+}
 
 const SearchPaperContainer = ({}) => {
+  const { store } = useStore();
+  const { papers, handleSearchPaper } = store.PaperStore;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [notFound, setNotFound] = useState<boolean>(false);
+  const [target, setTarget] = useState<string>("");
+
+  const requestHandleSearchPaper = useCallback(async () => {
+    setLoading(true);
+    await handleSearchPaper(target).then((res: SearchPaperResponse) => {
+      if (res.data.SearchedByName.length > 0 && res.data.SearchedByTitle.length > 0) {
+        setNotFound(false);
+      } else {
+        setNotFound(true);
+      }
+      setLoading(false);
+      console.log(papers);
+    });
+  }, []);
+
+  useEffect(() => {
+    requestHandleSearchPaper();
+  }, []);
+
   return (
     <>
-      <SearchPaper />
+      <SearchPaper papers={papers} loading={loading} notFound={notFound} setTarget={setTarget} />
     </>
   );
 };
