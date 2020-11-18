a88fd13e82b09360353cca7a1d23b7aabe32b928
Sh031224 <1cktmdgh2@gmail.com>
2020-11-18 18:05:22 +0900

    Update: CreatePaperPage

--- src/components/Paper/CreatePaper/CreatePaper.tsx
+++ src/components/Paper/CreatePaper/CreatePaper.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./CreatePaper.scss";
 import Button from "../../common/Button/Button";
 import { makeColor } from "../../../models/colorTemplate";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
+import { RiImageAddLine } from "react-icons/ri";
 
 interface CreatePaperProps {}
 
@@ -13,7 +16,6 @@ const CreatePaper = ({}: CreatePaperProps) => {
           <div className="create-paper-box-title">나만의 롤링페이퍼를 만들어보세요!</div>
           <div className="create-paper-box-content">
             <input className="create-paper-box-content-name" placeholder="제목" />
-            <input className="create-paper-box-content-info" placeholder="설명" />
             <div className="create-paper-box-content-range">
               <div className="create-paper-box-content-range-text">공개범위</div>
               <div className="create-paper-box-content-range-area">
@@ -35,11 +37,14 @@ const CreatePaper = ({}: CreatePaperProps) => {
               </div>
             </div>
             <div className="create-paper-box-content-date">
-              <div className="create-paper-box-content-date-text">기간</div>
+              <div className="create-paper-box-content-date-text">마감 시간</div>
+              <span className="create-paper-box-content-date-subtext">다른 사용자가 글을 작성 할 수 있는 기간입니다.</span>
               <div className="create-paper-box-content-date-area">
-                <input className="create-paper-box-content-date-area-input" type="date"></input>
-                <div className="create-paper-box-content-date-area-wave"> ~ </div>
-                <input className="create-paper-box-content-date-area-input" type="date"></input>
+                <DatePicker
+                  style={{ borderRadius: "4px", border: "1px solid #dcdcdc", color: "#707070" }}
+                  className="create-paper-box-content-date-area-input"
+                  showTime
+                />
               </div>
             </div>
             <div className="create-paper-box-content-backcolor">
@@ -48,6 +53,20 @@ const CreatePaper = ({}: CreatePaperProps) => {
                 {makeColor.map((color: string, index: number) => (
                   <div className="create-paper-box-content-backcolor-area-color" key={index} style={{ backgroundColor: color }} />
                 ))}
+              </div>
+            </div>
+            <div className="create-paper-box-content-thumbnail">
+              <div className="create-paper-box-content-date-text">썸네일</div>
+              <span className="create-paper-box-content-date-subtext">등록하지 않으면 기본 이미지로 자동 설정됩니다.</span>
+              <div className="create-paper-box-content-thumbnail-content">
+                <div className="create-paper-box-content-thumbnail-content-preview create-paper-box-content-thumbnail-content-preview-no">
+                  <div className="create-paper-box-content-thumbnail-content-upload">
+                    <label htmlFor="file">
+                      <RiImageAddLine />
+                    </label>
+                    <input id="file" type="file" accept="image/png, image/jpeg" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
