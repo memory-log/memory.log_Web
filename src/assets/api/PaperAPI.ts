bae3065d66a41fd48358b8bfc9b16c1804e5d541
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-18 15:29:13 +0900

    [UPDATE] Like Paper API 추가, Get Papers url 조건부 수정

--- src/assets/api/PaperAPI.ts
+++ src/assets/api/PaperAPI.ts
@@ -9,11 +9,11 @@ class PaperAPI {
       if (hit) {
         url += `?hit=${hit}`;
       }
-      
-      if (idx) {
-        url += `?paper_idx=${idx}`
-      } else if (code) {
-        url += `?code=${code}`
+
+      if (idx && code) {
+        url += `?paper_idx=${idx}&code=${code}`;
+      } else if (idx) {
+        url += `?paper_idx=${idx}`;
       }
 
       const { data } = await axios.get(url);
@@ -38,9 +38,21 @@ class PaperAPI {
 
   async SearchPaper(target: string) {
     try {
-      const url = `${SERVER}/paper/searchPaper/?target=${target}`;
+      const url = `${SERVER}/paper/searchPaper?target=${target}`;
 
       const { data } = await axios.get(url);
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async LikePaper(idx: number) {
+    try {
+      const url = `${SERVER}/paperLike/updateLike?paper_idx=${idx}`;
+
+      const { data } = await axios.post(url);
 
       return data;
     } catch (error) {
