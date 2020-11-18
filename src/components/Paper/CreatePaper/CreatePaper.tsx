186165496572b386f773ba19a6cfe7233457ac82
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-18 22:05:03 +0900

    [UPDATE] CreatePaper State 및 Component 수정

--- src/components/Paper/CreatePaper/CreatePaper.tsx
+++ src/components/Paper/CreatePaper/CreatePaper.tsx
@@ -6,30 +6,46 @@ import { DatePicker } from "antd";
 import "antd/dist/antd.css";
 import { RiImageAddLine } from "react-icons/ri";
 
-interface CreatePaperProps {}
+interface CreatePaperProps {
+  preview: string | ArrayBuffer | null;
+  setScope: React.Dispatch<React.SetStateAction<string>>;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  setEndTime: React.Dispatch<React.SetStateAction<Date>>;
+  setBackgroundColor: React.Dispatch<React.SetStateAction<string>>;
+  handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleCreatePaperCallback: () => Promise<void>;
+}
 
-const CreatePaper = ({}: CreatePaperProps) => {
+const CreatePaper = ({
+  preview,
+  setScope,
+  setTitle,
+  setEndTime,
+  setBackgroundColor,
+  handleImageChange,
+  handleCreatePaperCallback
+}: CreatePaperProps) => {
   return (
     <>
       <div className="create-paper">
         <div className="create-paper-box">
           <div className="create-paper-box-title">나만의 롤링페이퍼를 만들어보세요!</div>
           <div className="create-paper-box-content">
-            <input className="create-paper-box-content-name" placeholder="제목" />
+            <input className="create-paper-box-content-name" placeholder="제목" onChange={(e) => setTitle(e.target.value)} />
             <div className="create-paper-box-content-range">
               <div className="create-paper-box-content-range-text">공개범위</div>
               <div className="create-paper-box-content-range-area">
-                <label className="create-paper-box-content-range-area-select">
+                <label className="create-paper-box-content-range-area-select" onClick={() => setScope("PUBLIC")}>
                   전체공개
                   <input type="radio" name="range" />
                   <span className="create-paper-box-content-range-area-select-radio" />
                 </label>
-                <label className="create-paper-box-content-range-area-select">
+                <label className="create-paper-box-content-range-area-select" onClick={() => setScope("ONLY_CODE")}>
                   일부공개
                   <input type="radio" name="range" />
                   <span className="create-paper-box-content-range-area-select-radio" />
                 </label>
-                <label className="create-paper-box-content-range-area-select">
+                <label className="create-paper-box-content-range-area-select" onClick={() => setScope("PRIVATE")}>
                   나만보기
                   <input type="radio" name="range" />
                   <span className="create-paper-box-content-range-area-select-radio" />
@@ -44,6 +60,7 @@ const CreatePaper = ({}: CreatePaperProps) => {
                   style={{ borderRadius: "4px", border: "1px solid #dcdcdc", color: "#707070" }}
                   className="create-paper-box-content-date-area-input"
                   showTime
+                  onChange={(value, dateString) => setEndTime(new Date(dateString))}
                 />
               </div>
             </div>
@@ -51,7 +68,12 @@ const CreatePaper = ({}: CreatePaperProps) => {
               <div className="create-paper-box-content-backcolor-text">배경색</div>
               <div className="create-paper-box-content-backcolor-area">
                 {makeColor.map((color: string, index: number) => (
-                  <div className="create-paper-box-content-backcolor-area-color" key={index} style={{ backgroundColor: color }} />
+                  <div
+                    className="create-paper-box-content-backcolor-area-color"
+                    key={index}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setBackgroundColor(color)}
+                  />
                 ))}
               </div>
             </div>
@@ -59,19 +81,30 @@ const CreatePaper = ({}: CreatePaperProps) => {
               <div className="create-paper-box-content-date-text">썸네일</div>
               <span className="create-paper-box-content-date-subtext">등록하지 않으면 기본 이미지로 자동 설정됩니다.</span>
               <div className="create-paper-box-content-thumbnail-content">
-                <div className="create-paper-box-content-thumbnail-content-preview create-paper-box-content-thumbnail-content-preview-no">
+                <div
+                  className={
+                    preview
+                      ? "create-paper-box-content-thumbnail-content-preview"
+                      : "create-paper-box-content-thumbnail-content-preview create-paper-box-content-thumbnail-content-preview-no"
+                  }
+                >
+                  {preview && <img src={preview.toString()} className="create-paper-box-content-thumbnail-content-preview" />}
                   <div className="create-paper-box-content-thumbnail-content-upload">
                     <label htmlFor="file">
                       <RiImageAddLine />
                     </label>
-                    <input id="file" type="file" accept="image/png, image/jpeg" />
+                    <input id="file" type="file" accept="image/png, image/jpeg" onChange={(e) => handleImageChange(e)} />
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="create-paper-box-btnarea">
-            <Button text="롤링페이퍼 만들기" style={{ height: "3.6rem", fontSize: "1.2rem" }} />
+            <Button
+              text="롤링페이퍼 만들기"
+              style={{ height: "3.6rem", fontSize: "1.2rem" }}
+              onClick={() => handleCreatePaperCallback()}
+            />
           </div>
         </div>
       </div>
